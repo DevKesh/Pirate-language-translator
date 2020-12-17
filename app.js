bfaac65061efd1e24f065c3a2c486cc80456fe1a
Keshav <keshavnaganathan@gmai.com>
2020-12-17 17:44:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var buttonTranslate = document.querySelector("#button-translate");
+var textInput = document.querySelector("#text-input");
+var output = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("The Server is currently not working , Kindly try after sometime !!")
+}
+
+function clickHandler() {
+    var inputText = textInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        output.innerText = translatedText;
+    })
+    .catch(errorHandler)
+    
+};
+
+
+buttonTranslate.addEventListener("click", clickHandler)
